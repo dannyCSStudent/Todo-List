b69a6bc20a5ab666c7e239dc5be9f92cb2ef4dd0
Dan Villegas <danv8605@gmail.com>
2023-01-25 15:43:37 -0600

    Redid the design
    Added a responsive sidebar that will slide out
    when menu icon is clicked.
    Built PageBuiler.js to create all the elements
    to built the page.

--- src/PageBuilder.js
+++ src/PageBuilder.js
@@ -1,25 +1,56 @@
+import Icon from './images/menu.png';
+import Odin from './images/odin-lined.png';
 
 const buildPage = () => {
-    const content = document.querySelector('.content');
-    const header = document.createElement('div');
-    const hideSidebar = document.createElement('div');
-    const banner = document.createElement('div');
-    const addTaskBtn = document.createElement('button');
-   
-    header.classList.add('header');
-    hideSidebar.classList.add('hide-sidebar');
-    banner.classList.add('banner');
-    addTaskBtn.classList.add('add-task');
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    const myOdin = new Image();
+    myOdin.src = Odin;
 
-    hideSidebar.textContent = 'aaa';
-    banner.textContent = 'Todos';
-    addTaskBtn.textContent = 'Add';
+    
+    const body = document.querySelector('body');
+    const sidebar = document.createElement('div');
+    const mainContent = document.createElement('div');
+    const container = document.createElement('div');
+    const todo = document.createElement('h1');
+    const rightSide = document.createElement('h1');
+    const top = document.createElement('div');
+    const logo = document.createElement('div');
+    const user = document.createElement('div');
+    const btn = document.createElement('button');
+    const odinImg = document.createElement('img');
+    const menuImg = document.createElement('img');
 
-    header.appendChild(hideSidebar);
-    header.appendChild(banner);
-    header.appendChild(addTaskBtn);
+    odinImg.classList.add('odin-img');
+    btn.setAttribute('id', 'btn');
+    menuImg.classList.add('menu-img');
+    sidebar.classList.add('sidebar');
+    mainContent.classList.add('main-content');
+    container.classList.add('container');
+    
+    top.classList.add('top');
+    logo.classList.add('logo');
+    user.classList.add('user');
 
-    content.appendChild(header);
+    menuImg.setAttribute('src', myIcon.src);
+    todo.textContent = 'Todo List';
+    rightSide.textContent = 'Right Side'
+
+    container.appendChild(todo);
+    container.appendChild(rightSide);
+    mainContent.appendChild(container);
+
+;
+    user.appendChild(odinImg);
+    btn.appendChild(menuImg);
+    logo.appendChild(btn);
+    top.appendChild(logo);
+    sidebar.appendChild(top)
+    sidebar.appendChild(user);
+    body.appendChild(sidebar);
+    body.appendChild(mainContent);
+
+
 }
 
 export default buildPage
