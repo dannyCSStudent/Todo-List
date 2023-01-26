4eb2e6f5e6a354f7b12fc4feccd8fe6e48dff9cd
Dan Villegas <danv8605@gmail.com>
2023-01-26 15:41:47 -0600

    Added a button for the add a project input
    Add javascript to handle the click event.
    This event will first validate that an input
    is enetered. Then the parent element will be
    deleted. Then a new li is created.

--- src/index.js
+++ src/index.js
@@ -17,9 +17,51 @@ tooltip.addEventListener('click', function () {
     const li = document.createElement('li');
     const span = document.createElement('span');
     const input = document.createElement('input');
+    const addBtn = document.createElement('button');
+
     span.classList.add('nav-item');
     input.placeholder = 'Add a project';
+
+    addBtn.textContent = '+';
+    addBtn.classList.add('add-btn');
+
     span.appendChild(input);
     li.appendChild(span);
+    li.appendChild(addBtn);
     ul.appendChild(li);
+
+    let x = document.querySelector('input');
+    addBtn.addEventListener('click', function (e) {
+        let input = x.value;
+        if (!(validate(input))) {
+            alert('Please enter a project');
+            return
+        }
+        removeLi(e);
+        
+        createSpan(input, ul);
+    })
+
+    
+    
 })
+const validate = (data) => {
+    if (data == '') {
+        return false;
+    }
+    return true;
+}
+
+const removeLi = (e) => {
+    let target = e.target;
+    target.parentNode.parentNode.removeChild(target.parentNode);
+}
+
+const createSpan = (data, u) => {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = data;
+    li.appendChild(span);
+    u.appendChild(li);
+
+}
